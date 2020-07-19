3528937533a2d67f148fbf35cf14754263dea5dc
Paosder <denjaraos@gmail.com>
2020-07-19 18:43:47 +0900

    init

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const Iconv = require('iconv-lite');
+const fs = require('fs');
+const path = require('path');
+const { argv } = require('yargs');
+const chardet = require('chardet');
+
+Iconv.skipDecodeWarning = true;
+
+const kDefaultExt = ['.smi', '.ass', '.srt', '.vtt'];
+const availableEncodings = ['UTF-16LE', 'UTF-16BE', 'ANSI'];
+
+let exts = [];
+
+if (!argv.path) {
+    throw Error('No Path args!');
+}
+
+if (!argv.ext) {
+    exts = kDefaultExt;
+}
+
+fs.readdirSync(argv.path).forEach((filename) => {
+    const fullpath = path.join(argv.path, filename);
+    if (!fs.statSync(fullpath).isDirectory()) {
+        const extname = path.extname(fullpath);
+        if (extname !== '' && exts.includes(extname)) {
+            const encodingType = chardet.detectFileSync(fullpath);
+            if (availableEncodings.includes(encodingType)) {
+                console.log(filename);
+                const content = fs.readFileSync(fullpath, 'binary');
+                const encoded = Iconv.decode(content, encodingType);
+                fs.writeFileSync(fullpath, encoded, 'utf-8');
+            }
+        }
+    }
+});
