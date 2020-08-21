ea4bb719837837c07b785d2faacf9dfe495f7352
Paosder <denjaraos@gmail.com>
2020-08-22 00:43:25 +0900

    remove bin dir & add feature 'watch'

--- readme.md
+++ readme.md
@@ -1,3 +1,20 @@
 # to-utf8
 
 convert file encoding to utf-8.
+
+## Options
+
+### `path`
+
+Parse path.
+Required.
+
+### `watch`
+
+Watch directory.
+
+## How to use
+
+``` shell
+> ./main --path="c:\test" --watch
+```
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const { argv } = require('yargs');
 const chardet = require('chardet');
+const chokidar = require('chokidar');
 
 Iconv.skipDecodeWarning = true;
 
@@ -19,18 +20,40 @@ if (!argv.ext) {
     exts = kDefaultExt;
 }
 
-fs.readdirSync(argv.path).forEach((filename) => {
-    const fullpath = path.join(argv.path, filename);
-    if (!fs.statSync(fullpath).isDirectory()) {
-        const extname = path.extname(fullpath).toUpperCase();
-        if (extname !== '' && exts.includes(extname)) {
-            const encodingType = chardet.detectFileSync(fullpath).toUpperCase();
+
+if (argv.watch) {
+    console.log('go watcher mode...');
+    const watcher = chokidar.watch(argv.path);
+    const watcherFunc = (eventname, filePath, stats) => {
+        if (stats.isFile() && exts.includes(path.extname(filePath).toUpperCase())) {
+            const encodingType = chardet.detectFileSync(filePath).toUpperCase();
             if (availableEncodings.includes(encodingType)) {
-                console.log(filename);
-                const content = fs.readFileSync(fullpath, 'binary');
+                console.log(path.basename(filePath));
+                const content = fs.readFileSync(filePath, 'binary');
                 const encoded = Iconv.decode(content, encodingType);
-                fs.writeFileSync(fullpath, encoded, 'utf-8');
+                fs.writeFileSync(filePath, encoded, 'utf-8');
             }
         }
     }
-});
+    watcher.on('all', watcherFunc);
+    process.on('beforeExit', () => {
+        watcher.off('all', watcherFunc);
+    });
+} else {
+    fs.readdirSync(argv.path).forEach((filename) => {
+        const fullpath = path.join(argv.path, filename);
+        if (!fs.statSync(fullpath).isDirectory()) {
+            const extname = path.extname(fullpath).toUpperCase();
+            if (extname !== '' && exts.includes(extname)) {
+                const encodingType = chardet.detectFileSync(fullpath).toUpperCase();
+                if (availableEncodings.includes(encodingType)) {
+                    console.log(filename);
+                    const content = fs.readFileSync(fullpath, 'binary');
+                    const encoded = Iconv.decode(content, encodingType);
+                    fs.writeFileSync(fullpath, encoded, 'utf-8');
+                }
+            }
+        }
+    });
+    console.log('done.');
+}
